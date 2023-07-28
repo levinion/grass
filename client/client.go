cf5de65bee8897a1a1e010f91a7384a5bbe2e93a
levinion <levinion@163.com>
2023-07-28 23:50:22 +0800

    add retry to client

--- client/client.go
+++ client/client.go
@@ -4,20 +4,31 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 
 	"github.com/levinion/grass/message"
 )
 
 func SendMsg(command int, args string) {
-	conn, err := net.Dial("unix", message.SockAddr)
-	if err != nil {
-		fmt.Println("No server running...Quit now...")
-		os.Exit(0)
-	}
+	conn := retry(3)
 	conn.Write(message.BuildMsg(command, args).Marshal())
 	info := recvFeedback(conn)
 	fmt.Println(info)
 	conn.Close()
+}
+
+func retry(count int) net.Conn {
+	if count == 0 {
+		fmt.Println("No server running...Quit now...")
+		os.Exit(0)
+	}
+	conn, err := net.Dial("unix", message.SockAddr)
+	if err != nil {
+		// try every 0.1s
+		time.Sleep(time.Millisecond * 100)
+		return retry(count - 1)
+	}
+	return conn
 }
 
 func recvFeedback(conn net.Conn) string {
